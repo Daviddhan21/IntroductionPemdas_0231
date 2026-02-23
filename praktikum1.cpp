17909f9f539f584a813271a8f999aa7e71c9c551
Daviddhan21 <rayvanza.davida.ft24@mail.umy.ac.id>
2026-02-23 18:04:15 +0700

    first commit

---
+++ praktikum1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//variabel global
+int sks;
+
+int main(){
+    //variabel lokal
+    int umur;
+    umur = 20;
+
+    cout << "Halo Mas Vano" << endl;
+    cout << 10 << endl;
+    cout << umur;
+}
